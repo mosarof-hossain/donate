e499d42d6a74a2a7d8dc33e11a00a8a00a676376
mosarof-hossain <mosarof.hossain.cse@gmail.com>
2024-09-24 19:39:14 +0600

    donate calculation part complete

--- Js/index.js
+++ Js/index.js
@@ -1,9 +1,24 @@
-let totalDonate=0;
-function calculateDonate(){
-    const donateAmount=document.getElementById('donateAmount');
-    const amount=parseFloat(donateAmount.value);
-    totalDonate=totalDonate+amount;
-    document.getElementById('totalDonate').textContent=totalDonate+'BDT';
-    donateAmount.value='';
+let remainingBalance=5000;
+function calculateDonate(donateAmount, donateSum){
+        let donateA=parseFloat(donateSum.textContent) || 0;
+        let totalDonate=donateA+donateAmount;
+        remainingBalance=remainingBalance-donateAmount;
+        return {totalDonate, remainingBalance};
 }
-document.getElementById('donateNow').addEventListener('click',calculateDonate);
+
+document.querySelectorAll('.donateNow').forEach((button, index)=>{
+    button.addEventListener('click', function(){
+        const donateEntry=parseFloat(this.closest('.card-body').querySelector('.donateAmount').value);
+        const donateSum=document.querySelectorAll('.totalDonate')[index];
+        if(isNaN(donateEntry) || donateEntry<=0 || donateEntry>remainingBalance){
+            alert("Please enter valid amount");
+            return;
+        }
+
+        const {totalDonate, remainingBalance:balance}=calculateDonate(donateEntry, donateSum);
+        donateSum.textContent=totalDonate + 'BDT';
+        document.getElementById('mainBalance').textContent=balance+'BDT';
+        this.closest('.card-body').querySelector('.donateAmount').value='';
+
+    });
+});
